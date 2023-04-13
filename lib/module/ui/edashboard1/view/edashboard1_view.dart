daf3cf5c927d114f4a340c940a5ac2320b844a35
deny <deny@google.com>
2023-04-13 10:51:30 +0700

    .

--- lib/module/ui/edashboard1/view/edashboard1_view.dart
+++ lib/module/ui/edashboard1/view/edashboard1_view.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:magicbook/core.dart';
 import '../controller/edashboard1_controller.dart';
@@ -9,15 +10,251 @@ class Edashboard1View extends StatefulWidget {
     controller.view = this;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Edashboard1"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: const [],
+      // appBar: AppBar(
+      //   title: Text("Edashboard1"),
+      //   actions: [],
+      // ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text(
+                            "Shop",
+                            style: TextStyle(
+                              fontSize: 26.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 6.0,
+                          ),
+                          Text(
+                            "Over 45K Items Available for You",
+                            style: TextStyle(
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12.0,
+                    ),
+                    CircleAvatar(
+                      radius: 16.0,
+                      backgroundColor: Colors.grey,
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.grey[800],
+                        size: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                /*
+                MediaQuery
+                LayoutBuilder
+                OrientationBuilder
+                */
+
+                /*
+            TODO: Implement this @ controller
+            int currentIndex = 0;
+            final CarouselController carouselController = CarouselController();
+            */
+                Builder(builder: (context) {
+                  List images = [
+                    "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                    "https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+                    "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
+                    "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+                    "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80",
+                  ];
+
+                  return Column(
+                    children: [
+                      CarouselSlider(
+                        carouselController: controller.carouselController,
+                        options: CarouselOptions(
+                          height: 160.0,
+                          autoPlay: true,
+                          enlargeCenterPage: false,
+                          viewportFraction: 1.0,
+                          onPageChanged: (index, reason) {
+                            controller.currentIndex = index;
+                            controller.setState(() {});
+                          },
+                        ),
+                        items: images.map((imageUrl) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.amber,
+                                  borderRadius: const BorderRadius.all(
+                                    Radius.circular(6.0),
+                                  ),
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      imageUrl,
+                                    ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        }).toList(),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: images.asMap().entries.map((entry) {
+                          bool isSelected =
+                              controller.currentIndex == entry.key;
+                          return GestureDetector(
+                            onTap: () => controller.carouselController
+                                .animateToPage(entry.key),
+                            child: Container(
+                              width: isSelected ? 10.0 : 10.0,
+                              height: 10.0,
+                              margin: const EdgeInsets.only(
+                                right: 6.0,
+                                top: 12.0,
+                              ),
+                              decoration: BoxDecoration(
+                                color: isSelected
+                                    ? const Color(0xff59b58d)
+                                    : Colors.grey,
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(12.0),
+                                ),
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  );
+                }),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Row(
+                  children: const [
+                    Expanded(
+                      child: Text(
+                        "New Arrivals",
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "See All",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                      ),
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      size: 16.0,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 12.0,
+                ),
+                LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    bool isTablet = MediaQuery.of(context).size.width < 1100 &&
+                        MediaQuery.of(context).size.width >= 850;
+                    bool isDesktop = MediaQuery.of(context).size.width >= 1100;
+                    bool isMobile = MediaQuery.of(context).size.width < 850;
+
+                    var crossAxisCount = 2;
+                    if (isTablet) {
+                      crossAxisCount = 4;
+                    } else if (isDesktop) {
+                      crossAxisCount = 5;
+                    }
+
+                    return GridView.builder(
+                      padding: EdgeInsets.zero,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        childAspectRatio: 1.0 / 1.2,
+                        crossAxisCount: crossAxisCount,
+                        mainAxisSpacing: 10,
+                        crossAxisSpacing: 10,
+                      ),
+                      itemCount: controller.products.length,
+                      shrinkWrap: true,
+                      physics: const ScrollPhysics(),
+                      itemBuilder: (BuildContext context, int index) {
+                        var item = controller.products[index];
+                        return Container(
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                        item["photo"],
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                    borderRadius: const BorderRadius.all(
+                                      Radius.circular(
+                                        16.0,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                item["product_name"],
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 2.0,
+                              ),
+                              Text(
+                                item["category"],
+                                style: const TextStyle(
+                                  fontSize: 12.0,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/module/ui/elogin1/view/elogin1_view.dart
+++ lib/module/ui/elogin1/view/elogin1_view.dart
@@ -7,49 +7,197 @@ class Elogin1View extends StatefulWidget {
 
   Widget build(context, Elogin1Controller controller) {
     controller.view = this;
-
+    // Powertoys
+    // Windows+Shift+C
+    // Windows+Shift+T
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Elogin1"),
-        actions: const [],
-      ),
+      // appBar: AppBar(
+      //   title: Text("Elogin1"),
+      //   actions: [],
+      // ),
+      //ALT+SHIFT+S
       body: SingleChildScrollView(
+        controller: ScrollController(),
         child: Container(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
-                //alt+shift+enter
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Column(
-                    children: const [
-                      Text(
-                        "Welcome to MagicBook",
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
+                  Expanded(
+                    child: Column(
+                      children: const [
+                        Text(
+                          "Welcome to MagicBook",
+                          style: TextStyle(
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "Biggest collection of 300+ layouts for iOS prototyping.",
-                        style: TextStyle(
-                          fontSize: 12.0,
+                        SizedBox(
+                          height: 6.0,
                         ),
-                      ),
-                    ],
+                        Text(
+                          "Biggest collection of 300+ layouts for iOS prototyping.",
+                          style: TextStyle(
+                            fontSize: 12.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6.0,
                   ),
                   Image.network(
                     "https://images.unsplash.com/photo-1484517586036-ed3db9e3749e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                    width: 200.0,
-                    height: 200.0,
+                    width: 48.0,
+                    height: 48.0,
                     fit: BoxFit.cover,
                   ),
                 ],
+              ),
+              const SizedBox(
+                height: 80.0,
+              ),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(6.0),
+                  ),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: "Email",
+                    hintStyle: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              const SizedBox(
+                height: 8.0,
+              ),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[400],
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(6.0),
+                  ),
+                ),
+                child: TextFormField(
+                  decoration: InputDecoration.collapsed(
+                    hintText: "Password",
+                    hintStyle: TextStyle(
+                      color: Colors.grey[600],
+                    ),
+                  ),
+                  onChanged: (value) {},
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 38,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xff59b58d),
+                  ),
+                  onPressed: () {},
+                  child: const Text("Login"),
+                ),
+              ),
+              const SizedBox(
+                height: 6.0,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 38,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.red,
+                  ),
+                  onPressed: () {},
+                  child: const Text("Google"),
+                ),
+              ),
+              const SizedBox(
+                height: 6.0,
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 38,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  onPressed: () {},
+                  child: const Text("Facebook"),
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              const Text(
+                "Forgot password?",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff59b58d),
+                ),
               ),
             ],
           ),
         ),
       ),
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.all(12.0),
+        height: 60.0,
+        width: MediaQuery.of(context).size.width,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.grey,
+          ),
+          onPressed: () {},
+          child: Row(
+            children: [
+              Expanded(
+                child: Text(
+                  "Sign up",
+                  style: TextStyle(
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ),
+              Icon(
+                Icons.chevron_right,
+                size: 24.0,
+                color: Colors.grey[800],
+              ),
+            ],
+          ),
+        ),
+      ),
+      // bottomNavigationBar: Container(
+      //   height: 72,
+      //   width: MediaQuery.of(context).size.width,
+      //   padding: EdgeInsets.all(12.0),
+      //   child: ElevatedButton(
+      //     style: ElevatedButton.styleFrom(
+      //       backgroundColor: Colors.grey,
+      //     ),
+      //     onPressed: () {},
+      //     child: Text("Checkout"),
+      //   ),
+      // ),
     );
   }
 
